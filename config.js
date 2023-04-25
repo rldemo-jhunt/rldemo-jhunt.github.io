1eb723e97c35b31db25ec76dbad86cf3c6aabc1e
Josh Hunt <jdh96r@gmail.com>
2023-04-25 17:05:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.keycloakConfig = {
+const keycloakConfig = {
     url: 'https://localhost:8080/auth/realms/globex/protocol/openid-connect/auth',
     realm: 'globex',
     clientId: 'globex',
