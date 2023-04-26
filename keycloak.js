2f92d5a885b71a9ff006b62d103b8a3d878ab3f2
Josh Hunt <jdh96r@gmail.com>
2023-04-26 15:53:45 -0400

    Update keycloak.js

--- keycloak.js
+++ keycloak.js
@@ -1,16 +1,7 @@
-const keycloakConfig = {
-  url: 'http://localhost:8080/auth',
-  realm: 'globex',
-  clientId: 'globex',
-  redirectUri: "https://rldemo-jhunt.github.io/callback.html"
-};
+const keycloak = new Keycloak("./keycloak.json");
 
-const keycloak = new Keycloak(keycloakConfig);
-
-keycloak.init({
-  onLoad: 'login-required'
-}).success(function () {
-  // User is authenticated
-}).error(function () {
-  // User is not authenticated
+keycloak.init().then(function(authenticated) {
+  alert(authenticated ? 'authenticated' : 'not authenticated');
+}).catch(function() {
+  alert('failed to initialize');
 });
