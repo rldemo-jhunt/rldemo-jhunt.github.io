8db2c65a0d56b791373380d0c5c0a259d786ad58
Josh Hunt <jdh96r@gmail.com>
2023-04-27 12:27:56 -0400

    Update keycloak.js

--- keycloak.js
+++ keycloak.js
@@ -1,12 +1,12 @@
 var keycloak = new Keycloak("https://rldemo-jhunt.github.io/keycloak.json");
 
-//function login() {
+function login() {
   keycloak.init({ onLoad: 'login-required' }).then(function(authenticated) {
     alert(authenticated ? 'authenticated' : 'not authenticated');
   }).catch(function() {
     alert('failed to initialize');
   });
-//}
+}
 
 function logout() {
   //keycloak.logout('http://localhost:8080/auth/realms/globex/protocol/openid-connect/logout?redirect_uri=encodedRedirectUri')
