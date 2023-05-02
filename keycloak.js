1eec7370aa29c0c06e1c5efdc23f156eb069f2a1
Josh Hunt <jdh96r@gmail.com>
2023-05-02 12:18:18 -0400

    update

--- keycloak.js
+++ keycloak.js
@@ -1,6 +1,6 @@
 var keycloak = new Keycloak('./keycloak.json');
 
-function login() {
+//function login() {
   keycloak.init({
       onLoad: 'login-required'
     })
@@ -10,7 +10,7 @@ function login() {
     .catch(function() {
       console.error('Failed to initialize');
     });
-}
+//}
 
 function logout() {  
   keycloak.logout()
